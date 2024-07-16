4d7e28be06782a3d0f821b565b0017c928de441a
Komal Kant Millan <kkmillan29@gmail.com>
2024-07-16 18:40:15 +0530

    Added the code for flight_customer_gstin

--- flight_customer_gstin.py
+++ flight_customer_gstin.py
@@ -57,7 +57,7 @@ DAY_IN_MILLISECONDS = 86400000
 
 def getTransactionData(db, startepoch, endepoch, client_data_document, booking_type):
     try:
-        collection_data = db['mmt_data_test']
+        collection_data = db['mmt_data']
         client_id = client_data_document['expense_client_id']
         org_id = client_data_document['external_org_id']
         logging.info(f"Processing client {client_id} with org {org_id} for {booking_type}")
